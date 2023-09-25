5be4a9ce12e6935a6ca4e41d4576f0b745a3c35d
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-25 06:20:51 +0200

    house finished

--- src/pages/PageWelcome.ts
+++ src/pages/PageWelcome.ts
@@ -2,9 +2,9 @@ export const PageWelcome = () => {
 
 	class House {
 
-		public roofColor: string = '';
-		public doorColor: string = '';
-		public windowColor: string = '';
+		private roofColor: string = '';
+		private doorColor: string = '';
+		private windowColor: string = '';
 
 		constructor(roofColor: string, doorColor: string, windowColor: string) {
 			this.roofColor = roofColor;
@@ -12,18 +12,29 @@ export const PageWelcome = () => {
 			this.windowColor = windowColor;
 		}
 
-		describe() {
+		public getDoorColor(color: string) {
+			this.doorColor = color;
+		}
+
+		public describe() {
 			return `The roof is ${this.roofColor}, the door is ${this.doorColor} and the window is ${this.windowColor}.`;
+		}
+
+		static instantiateSimpleHouse(roofColor: string, doorColor: string) {
+			return new House(roofColor, doorColor, 'blue');
 		}
 	}
 
 	const house1 = new House('red', 'orange', 'blue');
 	const house2 = new House('blue', 'red', 'blue');
+	const house3 = House.instantiateSimpleHouse('red', 'orange');
 
 	return /*html*/`
 <div class="page pageWelcome">
-	<ul>
+	<ul class="list-disc ml-6">
 		<li>${house1.describe()}</li>
+		<li>${house2.describe()}</li>
+		<li>${house3.describe()}</li>
 	</ul>
 </div>
 `;
